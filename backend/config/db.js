750e225172829a3250c9a60923aa57f5e3a33f3f
10manny17 <echalumeau17@gmail.com>
2022-07-27 01:17:11 -0400

    Fixes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config()
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+  try {
+    const conn = await mongoose.connect("mongodb+srv://seniordesign:parkerstreet@seniordesignsum2022.1mzwc.mongodb.net/passwordmanager?retryWrites=true&w=majority")
 
-        console.log('MongoDB Connected: '.cyan.underline + conn.connection.host );
-    }
-    catch(error)
-        {
-            console.log(error)
-            process.exit(1)
-        }
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
 }
 
 module.exports = connectDB
