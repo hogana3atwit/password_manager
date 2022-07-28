398ee16748d35440f80dfa6a03ade6b6b2d19b5e
Andrew Hogan <hogana3@wit.edu>
2022-07-27 20:29:07 -0400

    Fixed routes

--- src/index.js
+++ src/index.js
@@ -5,24 +5,24 @@ import './font.css';
 import Home from '../src/Components/home/home';
 import App from './appholder';
 import { NavHeader } from './appholder.js';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PasswordGenerator, PasswordList } from "./appholder.js";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-	<Switch>
-	<Route exact path="/" component={Home}/>
-	</Switch>
-	<Switch>  
-        <Route exact path="/Dashboard" component={App}/>
-	</Switch>
-	<Switch>
-	<Route exact path="/password-list" component={PasswordList} />
-	</Switch>
-	<Switch>
-	<Route exact path="/password-generator" component={PasswordGenerator} />
-	</Switch>
+	<Routes>
+	<Route path="/" element={<Home/>}/>
+	</Routes>
+	<Routes>  
+        <Route path="/Dashboard" element={<App/>}/>
+	</Routes>
+	<Routes>
+	<Route path="/password-list" element={<PasswordList/>} />
+	</Routes>
+	<Routes>
+	<Route path="/password-generator" element={<PasswordGenerator/>} />
+	</Routes>
     </Router>
     </React.StrictMode>,
     document.getElementById('root')
